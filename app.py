bbfe212c9ac4e15978c6db2a3a1982f8d62308f1
Mehran Asaad <mehran.asaad@gmail.com>
2021-02-15 11:55:13 +0330

    fix #2 with using socketIO

--- app.py
+++ app.py
@@ -1,22 +1,41 @@
-from flask import Flask , request, render_template,redirect,url_for,jsonify
-from flask_sqlalchemy import SQLAlchemy
-import json
+from flask import Flask , request, render_template
 from threading import Thread
 import youtube_dl
+from flask_socketio import SocketIO, emit
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv("secret")
+socketio = SocketIO(app)
 
+percent = "0"
+filename = ""
+size = ""
+
+@socketio.on('bg-job')
+def handle_json(message):
+    global percent
+    global filename
+    global size
+
+    if float(percent) > 99:
+        resp = {"current": 99.99, "total": 100, "status":filename, "result":42, "finished":"True", "size":size}
+        emit('response',resp)
+    else:
+        resp = {"current": percent, "total": 100, "status":filename, "result":42}
+        emit('response',resp)
 
 def hook(d):
+    global filename 
     filename = d['filename'].replace("static/","").strip()
     if d['status'] == 'finished':
+        global size 
         size = d["_total_bytes_str"]
-        with open('static/result.txt','w') as file:
-            file.write(f"""{{"current": 99.99, "total": 100, "status":"{filename}","result": 42 , "finished":"True", "size":"{size}"}}""")
+
     if d['status'] == 'downloading':
+        global percent 
         percent = d['_percent_str'].replace('%','').strip()
-        with open('static/result.txt','w') as file:
-            file.write(f"""{{"current":"{percent}", "total": 100, "status":"{filename}","result": 42}}""")
+
 
 #Long_wrok
 def work(plink):
@@ -35,9 +54,6 @@ def work(plink):
 
 @app.route('/' , methods=['GET', 'POST'])
 def stream():
-
-    with open('static/result.txt','w') as file:
-        file.write("")
             
     if request.method == 'POST':
         plink = request.form['vidurl']
@@ -49,23 +65,6 @@ def stream():
     if request.method == 'GET':
         return render_template('index.html')
 
-@app.route('/status')
-def taskstatus():
-
-    data = ""
-    with open('static/result.txt','r') as file:
-        data = file.read()
-        if data !="":
-            response = json.loads(data)
-            return response
-        else:
-            return {
-            'state': 'PENDING',
-            'current': 1,
-            'total': 100,
-            'status': 'Pending...'
-        }
- 
  
 if __name__ == '__main__':
-    app.run(threaded=True)
+    socketio.run(app)
